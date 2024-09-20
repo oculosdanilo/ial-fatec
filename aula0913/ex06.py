89a00acb6d68a54c6f5bede75591214c0868432e
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2024-09-20 09:34:24 -0300

    Update ex06.py

--- aula0913/ex06.py
+++ aula0913/ex06.py
@@ -9,5 +9,6 @@ if limite >= 100:
     n += 1
 
   print(f"a soma de todos os números pares entre 1 e {limite} é {soma}")
+  # dica: usar o excel para testar valores altos
 else:
   print("o valor deve ser no mínimo 100 !") # correção não tão obrigatória: não só terminar o programa, mas pedir o valor certo novamente
