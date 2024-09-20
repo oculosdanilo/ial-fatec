afafa44bcf0deaaf54aa6dcb7f48c872ee82fb9e
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2024-09-20 09:23:01 -0300

    Update ex05.py

--- aula0913/ex05.py
+++ aula0913/ex05.py
@@ -1,6 +1,6 @@
 n = 1
 while n != 0:
-  n = int(input("digite um número inteiro: "))
+  n = int(input("digite um número inteiro: ")) # correção: input no final do while, para não aparecer "0 é divisível..."
 
   if n % 2 == 0 and n % 3 == 0:
     print(f"{n} é divisível por 2 e 3")
