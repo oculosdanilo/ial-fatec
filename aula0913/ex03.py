909ac49f8a99fd3d51894f8471a4f29ede31d1cf
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2024-09-20 08:55:11 -0300

    Update ex03.py

--- aula0913/ex03.py
+++ aula0913/ex03.py
@@ -3,7 +3,7 @@ quant = int(input("insira a quantidade de números reais: "))
 n = 0
 maior = 0
 menor = 0
-while n < quant: ' correção: conferir se o usuario inseriu 0
+while n < quant: # correção: conferir se o usuario inseriu 0
   real = float(input(f"insira o {n + 1}º número real: "))
 
   if real > maior:
