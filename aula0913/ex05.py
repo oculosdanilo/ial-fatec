c8554a5b74ab6124a9e646fd9d0f9bcca38fdc9c
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2024-09-20 09:15:22 -0300

    ops

--- aula0913/ex05.py
+++ aula0913/ex05.py
@@ -2,9 +2,9 @@ n = 1
 while n != 0:
   n = int(input("digite um número inteiro: "))
 
-  if n % 2 == 0 and n % 5 == 0:
-    print(f"{n} é divisível por 2 e 5")
+  if n % 2 == 0 and n % 3 == 0:
+    print(f"{n} é divisível por 2 e 3")
   elif n % 2 == 0:
     print(f"{n} é divisível por 2")
-  elif n % 5 == 0:
-    print(f"{n} é divisível por 5")
+  elif n % 3 == 0:
+    print(f"{n} é divisível por 3")
